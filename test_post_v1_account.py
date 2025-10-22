15fca1b4071c6cbe3afde7a19ea80860af061ff3
Kononirin <kononirin@gmail.com>
2025-10-22 20:33:38 +0300

    Add asserts. Add code of activation token.

--- test_post_v1_account.py
+++ test_post_v1_account.py
@@ -1,10 +1,11 @@
+from pprint import pprint
 import requests
+from json import loads
 
 
 def test_post_v1_account():
     # Регистрация пользователя
-
-    login = 'kirka'
+    login = 'kirka5'
     email = f'{login}@mail.ru'
     password = 'qwerty123'
 
@@ -18,8 +19,9 @@ def test_post_v1_account():
     print(response.status_code)
     print(response.text)
 
-    # Получить письма из почтового сервера
+    assert response.status_code == 201, "Пользователь не был создан"
 
+    # Получить письма из почтового сервера
     params = {
         'limit': '50',
     }
@@ -27,18 +29,32 @@ def test_post_v1_account():
     response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
     print(response.status_code)
     print(response.text)
+    # pprint(response.json())
+
+    assert response.status_code == 200, "Письма не были получены"
 
     # Получить активационный токен
-    ...
+    token = None
+    for item in response.json()['items']:
+        user_data = (loads(item['Content']['Body']))
+        user_login = user_data['Login']
+        if user_login == login:
+            print(user_login)
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            print(token)
+
+    assert token is not None, f"Токен для пользователя {login} не был получен"
 
     # Активация пользователя
     headers = {
         'accept': 'text/plain',
     }
 
-    response = requests.put('http://5.63.153.31:5051/v1/account/7264c537-a407-4db3-8879-ba7122f24941', headers=headers)
+    response = requests.put(f'http://5.63.153.31:5051/v1/account/{token}', headers=headers)
     print(response.status_code)
     print(response.text)
+
+    assert response.status_code == 200, "Пользователь не был активирован"
 
     # Авторизоваться
     json_data = {
@@ -50,3 +66,5 @@ def test_post_v1_account():
     response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
     print(response.status_code)
     print(response.text)
+
+    assert response.status_code == 200, "Пользователь не смог авторизоваться"
