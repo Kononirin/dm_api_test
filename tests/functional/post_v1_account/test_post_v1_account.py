8401609bd92977e7d22e22ca753ffce90916bb88
Kononirin <kononirin@gmail.com>
2025-10-30 18:23:52 +0300

    Add configuration.py

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -4,6 +4,8 @@ from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 import structlog
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
 
 structlog.configure(
     processors=[
@@ -17,11 +19,14 @@ structlog.configure(
 
 def test_post_v1_account():
     # Регистрация пользователя
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    login = 'kirka_post_19'
+    account_api = AccountApi(dm_api_configuration)
+    login_api = LoginApi(dm_api_configuration)
+    mailhog_api = MailhogApi(mailhog_configuration)
+
+    login = 'kirka_post_21'
     email = f'{login}@mail.ru'
     password = 'qwerty123'
 
