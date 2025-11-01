09244c9ea5e1c33f0f1b71529f74a5f301e405c9
Irina Sh. <104162505+Kononirin@users.noreply.github.com>
2025-11-01 19:56:42 +0300

    Rest client proxy add (#3)
    * Add structure. Create functions in clients classes.
    
    * Add github-actions-job.
    
    * Add github-actions-job.
    
    * Fix mistake in python.yml
    
    * Fix mistake in requirements.txt
    
    * Fix comand in worflows
    
    * Fix comand in workflow
    
    * Change login. Add new data.
    
    * Add tests test_put_v1_account_token(), test_put_v1_account_login(), test_put_v1_account_email()
    
    * Add pattern proxe
    
    * Add pattern proxy, add curl
    
    * Add configuration.py
    
    * Add configuration.py
    
    * Resolve problem in tests on github
    
    * Fix requirements.txt
    
    * Add changes in all tests
    
    * Send new test data
    
    * Send new test data try №2

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -3,15 +3,30 @@ from json import loads
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+import structlog
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
 
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(
+            indent=4,
+            ensure_ascii=True,
+            #sort_keys=True
+        )
+    ]
+)
 
 def test_post_v1_account():
     # Регистрация пользователя
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    login = 'kirka_post_12'
+    account_api = AccountApi(dm_api_configuration)
+    login_api = LoginApi(dm_api_configuration)
+    mailhog_api = MailhogApi(mailhog_configuration)
+
+    login = 'kirka_post_27'
     email = f'{login}@mail.ru'
     password = 'qwerty123'
 
@@ -22,15 +37,11 @@ def test_post_v1_account():
     }
 
     response = account_api.post_v1_account(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
 
     assert response.status_code == 201, "Пользователь не был создан"
 
     # Получить письма из почтового сервера
     response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
-    print(response.text)
 
     assert response.status_code == 200, "Письма не были получены"
 
@@ -41,8 +52,6 @@ def test_post_v1_account():
 
     # Активация пользователя
     response = account_api.put_v1_account_token(token=token)
-    print(response.status_code)
-    print(response.text)
 
     assert response.status_code == 200, "Пользователь не был активирован"
 
@@ -54,12 +63,13 @@ def test_post_v1_account():
     }
 
     response = login_api.post_v1_account_login(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
 
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
 
-def get_activation_token_by_login(login, response):
+def get_activation_token_by_login(
+        login,
+        response
+        ):
     token = None
     for item in response.json()['items']:
         user_data = (loads(item['Content']['Body']))
@@ -67,7 +77,3 @@ def get_activation_token_by_login(login, response):
         if user_login == login:
             token = user_data['ConfirmationLinkUrl'].split('/')[-1]
     return token
-
-
-
-
