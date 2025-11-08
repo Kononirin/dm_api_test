63198fea45f4128854eca94db99f32933bd2c779
Kononirin <kononirin@gmail.com>
2025-11-08 18:56:33 +0300

    Add context_manager

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -10,34 +10,37 @@ from hamcrest import (
     equal_to,
 )
 
+from checkers.http_checkers import check_status_code_http
+
 
 def test_get_v1_account_auth(
         auth_account_helper
 ):
-    response = auth_account_helper.get_account_info(True)
-    assert_that(
-        response, all_of(
-            has_property('resource', has_property('login', starts_with('kirka'))),
-            has_property('resource', has_property('registration', instance_of(datetime))),
-            has_property(
-                'resource', has_properties(
-                    {
-                        'rating': has_properties(
-                            {
-                                "enabled": equal_to(True),
-                                "quality": equal_to(0),
-                                "quantity": equal_to(0)
-                            }
-                        )
-                    }
+    with check_status_code_http():
+        response = auth_account_helper.get_account_info(True)
+        assert_that(
+            response, all_of(
+                has_property('resource', has_property('login', starts_with('kirka'))),
+                has_property('resource', has_property('registration', instance_of(datetime))),
+                has_property(
+                    'resource', has_properties(
+                        {
+                            'rating': has_properties(
+                                {
+                                    "enabled": equal_to(True),
+                                    "quality": equal_to(0),
+                                    "quantity": equal_to(0)
+                                }
+                            )
+                        }
+                    )
                 )
             )
         )
-    )
 
 
 def test_get_v1_account_no_auth(
         account_helper
 ):
-    response = account_helper.get_account_info()
-    assert response.status_code == 401, "Пользователь успешно авторизовался"
+    with check_status_code_http(401, "User must be authenticated"):
+        account_helper.get_account_info()
