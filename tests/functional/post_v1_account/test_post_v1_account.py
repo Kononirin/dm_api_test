2c35f31a20e666380944bb4f954160385836d0de
Irina Sh. <104162505+Kononirin@users.noreply.github.com>
2025-11-13 14:17:53 +0300

    Context manager (#17)
    * Get branche pydantic actual v4
    
    * Add reset_password model
    
    * Add change_password model
    
    * Add change_email model
    
    * Add pydantic models for all tests
    
    * Add asserts hamcrest in test_get_v`_account
    
    * Add context_manager
    
    * Add context manager

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import pytest
 from hamcrest import (
     assert_that,
     has_property,
@@ -9,7 +10,9 @@ from hamcrest import (
     has_properties,
     equal_to,
 )
-
+from checkers.http_checkers import (
+    check_status_code_http
+)
 
 def test_post_v1_account(
         account_helper,
@@ -40,3 +43,27 @@ def test_post_v1_account(
             )
         )
     )
+
+
+@pytest.mark.parametrize(
+    'login, password, email, expected_message', [
+        # Короткий пароль
+        ("valid_login", "qwe", "valid@email.com", "Validation failed"),
+        # Короткий логин
+        ("q", "valid_password", "valid@email.com", "Validation failed"),
+        # Некорректный email
+        ("valid_login", "valid_password", "invalid_email", "Validation failed")]
+)
+def test_post_v1_account_negative_invalid_credentials(
+        account_helper,
+        login,
+        password,
+        email,
+        expected_message
+
+):
+    with check_status_code_http(400, expected_message):
+        account_helper.register_new_user(login=login, password=password, email=email)
+
+    with check_status_code_http(400, "One or more validation errors occurred."):
+        account_helper.user_login(login=login, password=password, validate_response=True)
--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -1,8 +1,10 @@
+from checkers.http_checkers import check_status_code_http
+
+
 def test_put_v1_account_email(
         account_helper,
         prepare_user
-        ):
-
+):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
@@ -11,8 +13,8 @@ def test_put_v1_account_email(
     response = account_helper.user_login(login=login, password=password)
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
     account_helper.change_user_email(login=login, password=password, email=email)
-    response = account_helper.user_login(login=login, password=password)
-    assert response.status_code == 403, "Пользователь авторизовался"
+    with check_status_code_http(403, "User is inactive. Address the technical support for more details"):
+        account_helper.user_login(login=login, password=password)
     token = account_helper.get_token_by_login(login=login)
     account_helper.activate_user_by_token(token)
     account_helper.user_login(login=login, password=password)
