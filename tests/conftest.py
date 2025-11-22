08d43857f09a55fbb586a4ad53ed9269dfcf19d7
Kononirin <kononirin@gmail.com>
2025-11-22 10:45:24 +0300

    Refactor python.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,7 +41,7 @@ def setup_swagger_coverage():
     yield
     reporter.generate_report()
     reporter.cleanup_input_files()
-    send_file()
+    # send_file()
 
 @pytest.fixture(scope="session", autouse=True)
 def set_config(request):
